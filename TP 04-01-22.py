79f21ed627e832220531d8628321e5ce2f8334a2
YapYap31 <yapyap880@gmail.com>
2022-01-05 09:28:37 +0100

    Update TP 04-01-22
    Exo 1 terminé

--- TP 04-01-22.py
+++ TP 04-01-22.py
@@ -1,3 +1,5 @@
+# 1 branche github par fonctionnalité
+
 import re
 
 nom = input("Quel est votre nom ? \n")
@@ -10,40 +12,28 @@ if annee > 2022 - 6:
     print("#####################")
 elif annee > 2022 - 12:
     print("#####################")
-    print("Vous êtes poussin")
+    print("Vous êtes catégorie : poussin")
     print("#####################")
 elif annee > 2022 - 18:
     print("#####################")
-    print("Vous êtes cadet")
+    print("Vous êtes catégorie : cadet")
     print("#####################")
 elif annee > 2022 - 24:
     print("#####################")
-    print("Vous êtes junior")
+    print("Vous êtes catégorie : junior")
     print("#####################")
 elif annee > 2022 - 30:
     print("#####################")
-    print("Vous êtes semi-pro")
+    print("Vous êtes catégorie : semi-pro")
     print("#####################")
 elif annee >= 2022 - 40:
     print("#####################")
-    print("Vous êtes pro")
+    print("Vous êtes catégorie : pro")
     print("#####################")
 else:
     print("#####################")
     print("Vous n'êtes pas admis")
     print("#####################")
 
-def verif_mail(adresse):
-    rex = re.compile("[A-Z]{1}.[a-z]{6}@baton-rouge.fr")
-    if rex.fullmatch(adresse):
-        return True
-    else:
-        return False
-
-mail = input("Quelle est votre adresse mail ? \n")
-
-if verif_mail(mail):
-    print("OK")
-    print(f"Nom : {(nom)} ; Prénom : {(prenom)} ; Mail : {(mail)} ")
-else:
-    print("Ecrivez correctement votre adresse mail ! modèle : H.potter@baton-rouge.fr")
+print(f"Nom : {(nom)} ; Prénom : {(prenom)} ; Mail : {str.capitalize(prenom[0])}.{(nom)}@baton-rouge.fr")
+print("#####################")
